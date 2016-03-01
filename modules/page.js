b9a5d7a13a496b758acef8beecfb92e00ed03a94
Sergey 'Hoichi' Samokhov <samokhov@gmail.com>
2016-03-02 00:58:22 +0300

    pageFromSource is written! (yet to try to run it though)

--- modules/page.js
+++ modules/page.js
@@ -1,48 +1,48 @@
 'use strict';
 
-const   fm      = require('front-matter'),
-        fs      = require('fs'),
-        _       = require('lodash'),    // todo: https://medium.com/making-internets/why-using-chain-is-a-mistake-9bc1f80d51ba#.azak8kbwc
-        mPath    = require('path'),
-        u       = require('./utils.js'),
-        url     = require('url');
+const fm = require('front-matter'),
+    fs = require('fs'),
+    _ = require('lodash'),    // todo: https://medium.com/making-internets/why-using-chain-is-a-mistake-9bc1f80d51ba#.azak8kbwc
+    mPath = require('path'),
+    u = require('./utils.js'),
+    url = require('url');
 
 /*
-  Jekyll vars
-  ===========
+ Jekyll vars
+ ===========
 
-page.content
+ page.content
  The content of the Page, rendered or un-rendered depending upon what Liquid is being processed and what page is.
 
-page.title
+ page.title
 
-page.excerpt
+ page.excerpt
  The un-rendered excerpt of the Page.
 
-page.url
+ page.url
  The URL of the Post without the domain, but with a leading slash, e.g. /2008/12/14/my-post.html
 
-page.date
+ page.date
  The Date assigned to the Post. This can be overridden in a Post’s front matter by specifying a new date/time in the format YYYY-MM-DD HH:MM:SS (assuming UTC), or YYYY-MM-DD HH:MM:SS +/-TTTT (to specify a time zone using an offset from UTC. e.g. 2008-12-14 10:30:00 +0900).
 
-page.id
+ page.id
  An identifier unique to the Post (useful in RSS feeds). e.g. /2008/12/14/my-post
 
-page.categories
+ page.categories
  The list of categories to which this post belongs. Categories are derived from the directory structure above the _posts directory. For example, a post at /work/code/_posts/2008-12-24-closures.md would have this field set to ['work', 'code']. These can also be specified in the YAML Front Matter.
 
-page.tags
+ page.tags
  The list of tags to which this post belongs. These can be specified in the YAML Front Matter.
 
-page.path
+ page.path
  The path to the raw post or page. Example usage: Linking back to the page or post’s source on GitHub. This can be overridden in the YAML Front Matter.
 
-page.next
+ page.next
  The next post relative to the position of the current post in site.posts. Returns nil for the last entry.
 
-page.previous
+ page.previous
 
-*/
+ */
 
 /*
  function drawES6Chart({size = 'big', cords = { x: 0, y: 0 }, radius = 25} = {}) {
@@ -58,7 +58,14 @@ page.previous
  cords: { x: 18, y: 30 },
  radius: 30
  });
-*/
+ */
+
+const defaultMetaConverters = {
+    date: d => new Date(d),
+    published: p => p === undefined || p,
+    tags: t => ( t && t.split(/(,|\s)+/g) ) || [],
+    categories: c => c && (c && c.split(/(,|\s)+/g)) || []
+};
 
 function PageConstructorFabric() {
     function PageConstructor() {
@@ -67,10 +74,10 @@ function PageConstructorFabric() {
         }
 
         /*  fixme:
-            Do we need a constructor separate from fromSource/fromSourceSync?
-            Sure, we do need that [new] boilerplate and all that prototype stuff.
-            But fromSource[...] is a constructor and should be a module property, pageFromSource or smth.
-        */
+         Do we need a constructor separate from fromSource/fromSourceSync?
+         Sure, we do need that [new] boilerplate and all that prototype stuff.
+         But fromSource[...] is a constructor and should be a module property, pageFromSource or smth.
+         */
 
         return this;
     }
@@ -79,33 +86,34 @@ function PageConstructorFabric() {
         setProperties: {
             enumerable: true,
             value: (data = {}) => {
-                data.keys.forEach((val, key) => {
+                Object.keys(data).forEach((val, key) => {
                     this[key] = val;    // fixme: use defineProperty?
                 });
             }
         },
         fromSourceSync: {
             enumerable: true,
-            value:
-            (   path,
+            value: (
+                path,
                 {   /* here be options */
-                    contentReader = raw => {let {attributes: meta, body} = fm(raw); return {meta, body};},
+                    contentReader = raw => {
+                        let {attributes: meta, body} = fm(raw);
+                        return {meta, body};
+                    },
                     cwd = process.cwd(),
                     encoding = 'UTF-8',
                     extensions = '*',
                     markupConverter = plainTextToHtml,
-                    metaConverters: {
-                        date = d => new Date(d),
-                        published = p => p === undefined || p,
-                        tags = t => ( t && t.split(/(,|\s)+/g) ) || [],
-                        categories = c => c && (c && c.split(/(,|\s)+/g)) || []
+                    metaConverters = {},
+                    metaFromPath = (path, pathObj) => {
+                        return {};
                     }
                 }
             ) => {
                 try {
-                    let {dirs, ext, name, rel} = parsePath(path, cwd),
+                    let pathParsed = parsePath(path, cwd),
                         {meta, body: srcBody} = runFileReader(path, contentReader, {encoding}),
-                        content, excerpt;
+                        content, excerpt, combinedMetaConverters;
 
                     if (!meta.title || !meta.date || !srcBody) {
                         throw Error(`A page has to have at least a title, date and some content.
@@ -115,28 +123,25 @@ function PageConstructorFabric() {
                     // convert content
                     ({content, excerpt} = runMarkupConverter(srcBody, markupConverter));
 
-                    this.content = content;
-                    this.excerpt = excerpt;
+                    combinedMetaConverters = Object.assign({}, defaultMetaConverters, metaConverters);
 
-                    // todo: create and run metaFromPath
-                    // todo: `run metaConverters
-
-                    // this.url should probably set when we call page.render()
-
-                    /* todo: Categories (Jekyll: /work/code/... -> ['work', 'code']. Should we need more flexibility?) */
-                    //  todo: set getters for url, next etc. that says it's not initialized yet. and don't fucking freeze 'em
-                    //    todo: site, prev/next, url (callback?)
-
-
-                } catch(e) {
-                    throw e;
+                    Object.assign(
+                        this,
+                        {excerpt},  // it's fine to redefine the excerpt in front-matter
+                        metaFromPath(path, pathParsed), // todo: check if an object is returned
+                        runMetaConverters(meta, combinedMetaConverters),
+                        {content}   // but we're probably safer not redefining content
+                    );
+                } catch (e) {
+                    throw Error(`Failed creating a page from ${path}.\nError message runs: ‘${e.message}’`);
                 }
 
                 return this;
             }
         },
         next: {
-            get: () => {/* todo: or should it be a simple value set by .collect()? */}
+            get: () => {/* todo: or should it be a simple value set by .collect()? */
+            }
         }
     });
 
@@ -163,15 +168,15 @@ function firstParagraph(html) {
 }
 
 /*
-* Takes a path (and a working dir)
-* returns an object with:
-* - file base (sans extension)
-* - an arr of parent dirs
-* */
+ * Takes a path (and a working dir)
+ * returns an object with:
+ * - file base (sans extension)
+ * - an arr of parent dirs
+ * */
 function parsePath(path, cwd) {
 // todo: should we move it to Utils?
 // todo: check if file actually exists? or is senseless if we get it from Glob or smth? it kinda should fail gracefuly if it't removed by the time we get here
-    let {root, dir, base, ext, name} = mPath.parse( mPath.resolve(path) ),
+    let {root, dir, base, ext, name} = mPath.parse(mPath.resolve(path)),
         rel = path.relative(cwd, dir),
         dirs = rel.split(mPath.sep);
 
@@ -209,8 +214,12 @@ function runFileReader(path, reader, {encoding}) {
 
     parsed = reader(raw);
 
-    if (!parsed.meta) { throw Error (`No metadata in file ${path}`); }
-    if (!parsed.body) { throw Error (`No text in file ${path}`); }
+    if (!parsed.meta) {
+        throw Error(`No metadata in file ${path}`);
+    }
+    if (!parsed.body) {
+        throw Error(`No text in file ${path}`);
+    }
 
     return parsed;
 }
@@ -226,8 +235,27 @@ function readTextWithYfm(path, {encoding = 'UTF-8'}) {
 
     ({attributes: meta, body} = fm(content));
 
-    if (!meta) { throw Error (`No metadata in file ${path}`); }
-    if (!body) { throw Error (`No text in file ${path}`); }
+    if (!meta) {
+        throw Error(`No metadata in file ${path}`);
+    }
+    if (!body) {
+        throw Error(`No text in file ${path}`);
+    }
 
     return {meta, body};
+}
+
+function runMetaConverters(meta, converters, includeUnconverted = true) {
+    let result = {},
+        conv;
+
+    Object.keys(meta).forEach((val, key) => {
+        if (conv = converters[key]) {
+            result[key] = conv(val);
+        } else if (includeUnconverted) {
+            result[key] = val;
+        }
+    });
+
+    return result;
 }
